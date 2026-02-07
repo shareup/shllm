d7d35cdf26a6a32a4e01702300a926ccf996669c
Anthony Drendel <me@anthonydrendel.com>
2026-02-07 23:25:32 +0100

    Update to mlx-swift-lm to v0.0.2

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/shareup/mlx-swift-lm",
-            from: "0.0.1"
+            from: "0.0.2"
         ),
         .package(
             url: "https://github.com/huggingface/swift-transformers",
