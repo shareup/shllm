6b1bc38ef8fbc79cb875bdc0ec0f032acf9e3ff9
Anthony Drendel <me@anthonydrendel.com>
2026-04-06 14:26:01 +0200

    Update mlx-swift-lm

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/shareup/mlx-swift-lm",
-            from: "0.0.13"
+            from: "0.0.14"
         ),
         .package(
             url: "https://github.com/DePasqualeOrg/swift-tokenizers",
